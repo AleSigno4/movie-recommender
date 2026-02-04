b03e2c08c3c78cb53de262ef76fad418d9a980dc
Alessandro Signori <alesignori4@gmail.com>
2026-02-04 10:00:13 +0100

    fix: Initiliazation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/pages/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+    "./src/styles/**/*.css",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

ad50e3a61a8b4c0aafab04856cac0ba355abcb07
Alessandro Signori <alesignori4@gmail.com>
2026-02-06 09:58:34 +0100

    feat: Creation of the model

--- backend/app/api/movies.py
+++
@@ -1,9 +0,0 @@
-class Movie():
-    def __init__(self, id: int, title: str, genres: list, url: str, year: int, rating: float):
-        self.id = id
-        self.title = title
-        self.genres = genres
-        self.poster_url = url
-        self.year = year
-        self.rating = rating
-    

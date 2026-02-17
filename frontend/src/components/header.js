d71898ff01bf4e7ad8648fe489bda4b758c33ee0
Alessandro Signori <alesignori4@gmail.com>
2026-02-17 11:30:24 +0100

    fix: Header width

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <Link href="/">
-      <div className="flex mb-0">
-        <i className="fas fa-film fa-3x text-orange-400 m-5"></i>
-        <h1 className="text-4xl font-bold text-orange-400 my-5 mx-0">Film<span className="text-white">Finder</span></h1>
-      </div>
+    <Link href="/" className="flex items-center w-fit">
+      <i className="fas fa-film fa-3x text-orange-400 m-5"></i>
+      <h1 className="text-4xl font-bold text-orange-400 my-5">
+        Film<span className="text-white">Finder</span>
+      </h1>
     </Link>
   );
 }

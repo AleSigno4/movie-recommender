8b831d9c5e11d0836229edbffe227f2ae915e106
Alessandro Signori <alesignori4@gmail.com>
2026-02-18 12:49:06 +0100

    feat: Add icon page

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,6 +1,16 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Movie Recommender</title>
+        <meta name="description" content="Discover your next favorite movie" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
